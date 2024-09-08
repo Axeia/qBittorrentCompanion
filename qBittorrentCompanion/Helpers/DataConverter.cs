ece512830d1855db8f8322775b1c5a0e18190478
Pascal Bakhuis <pbakhuis@gmail.com>
2024-09-08 23:45:50 +0100

    Added GetMultiplierForUnit so it's accessible to other classes.

--- qBittorrentCompanion/Helpers/DataConverter.cs
+++ qBittorrentCompanion/Helpers/DataConverter.cs
@@ -29,6 +29,28 @@ namespace qBittorrentCompanion.Helpers
             }
         }
 
+        public static long GetMultiplierForUnit(string sizeUnit)
+        {
+            switch (sizeUnit)
+            {
+                case "B":
+                default:
+                    return 1L;
+                case "KiB":
+                    return 1024L; // 1 KiB = 1024 B
+                case "MiB":
+                    return 1024L * 1024L; // 1 MiB = 1024 KiB
+                case "GiB":
+                    return 1024L * 1024L * 1024L; // 1 GiB = 1024 MiB
+                case "TiB":
+                    return 1024L * 1024L * 1024L * 1024L; // 1 TiB = 1024 GiB
+                case "PiB":
+                    return 1024L * 1024L * 1024L * 1024L * 1024L; // 1 PiB = 1024 TiB
+                case "EiB":
+                    return 1024L * 1024L * 1024L * 1024L * 1024L * 1024L; // 1 EiB = 1024 PiB
+            }
+        }
+
         public static string StateToHumanReadable(TorrentState state)
         {
             switch(state)
