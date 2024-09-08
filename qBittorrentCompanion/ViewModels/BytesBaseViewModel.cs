49bfecba1635554aef8807f107495409fa9d2cd7
Pascal Bakhuis <pbakhuis@gmail.com>
2024-09-08 23:44:51 +0100

    Added GetMultiplierForUnit method so it can be moved out of BytesBase

--- qBittorrentCompanion/ViewModels/BytesBaseViewModel.cs
+++ qBittorrentCompanion/ViewModels/BytesBaseViewModel.cs
@@ -17,28 +17,6 @@ namespace qBittorrentCompanion.ViewModels
     {
         public static string[] SizeOptions => ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"];
 
-        public long GetMultiplierForUnit(string sizeUnit)
-        {
-            switch (sizeUnit)
-            {
-                case "B":
-                default:
-                    return 1L;
-                case "KiB":
-                    return 1024L; // 1 KiB = 1024 B
-                case "MiB":
-                    return 1024L * 1024L; // 1 MiB = 1024 KiB
-                case "GiB":
-                    return 1024L * 1024L * 1024L; // 1 GiB = 1024 MiB
-                case "TiB":
-                    return 1024L * 1024L * 1024L * 1024L; // 1 TiB = 1024 GiB
-                case "PiB":
-                    return 1024L * 1024L * 1024L * 1024L * 1024L; // 1 PiB = 1024 TiB
-                case "EiB":
-                    return 1024L * 1024L * 1024L * 1024L * 1024L * 1024L; // 1 EiB = 1024 PiB
-            }
-        }
-
         protected override Task UpdateDataAsync(object? sender, ElapsedEventArgs e)
         {
             throw new NotImplementedException();
