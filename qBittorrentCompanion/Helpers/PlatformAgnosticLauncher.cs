f1f2a9d355ac86254b3fab19cf769c0902a729fd
Pascal Bakhuis <pbakhuis@gmail.com>
2024-10-06 17:03:18 +0100

    Added functionality for launching folders/files

--- qBittorrentCompanion/Helpers/PlatformAgnosticLauncher.cs
+++ qBittorrentCompanion/Helpers/PlatformAgnosticLauncher.cs
@@ -31,5 +31,54 @@ namespace qBittorrentCompanion.Helpers
                 }
             }
         }
+        public static void OpenFile(string absolutePath)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                Debug.WriteLine($"Opening explorer with file: {absolutePath}");
+                if (File.Exists(absolutePath))
+                {
+                    Process.Start("explorer.exe", absolutePath);
+                }
+                else
+                {
+                    Debug.WriteLine($"{absolutePath} doesn't exist! Unable to launch");
+                }
+            }
+        }
+
+        /// <summary>
+        /// <list type="bullet">
+        /// <item>
+        /// <term>The given path leads to a file</term>
+        /// <description>open the containing directory in the explorer with the file pre-selected.</description></item>
+        /// <item>
+        /// <term>The given path is a directory</term>
+        /// <description>open it in the explorer</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="absolutePath">Path to a location accessible on this system</param>
+        public static void OpenDirectoryAndSelectFile(string absolutePath)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                Debug.WriteLine($"Opening explorer with: {absolutePath}");
+                if (File.Exists(absolutePath))
+                {
+                    Process.Start("explorer.exe", "/select, \"" + absolutePath + "\"");
+                }
+                else if (Directory.Exists(absolutePath))
+                {
+                    Process.Start("explorer.exe", absolutePath);
+                }
+                else
+                {
+                    Debug.WriteLine($"{absolutePath} doesn't exist! Unable to launch");
+                }
+
+            }
+        }
     }
+
 }
