5cf5ec7c7f8175dc67a192142a700305989ca8b0
Pascal Bakhuis <pbakhuis@gmail.com>
2024-11-05 22:17:25 +0000

    Converter added for converting Avalonias Color to hex back and forth

---
+++ qBittorrentCompanion/Converters/ColorToHexConverter.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace qBittorrentCompanion.Converters
+{
+    public class ColorToHexConverter : IValueConverter
+    {
+        public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is Color color)
+                return ColorToHex(color);
+
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is string hex && Color.TryParse(hex, out var color))
+                return color;
+
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        private string ColorToHex(Color color) 
+            => $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+    }
+}
