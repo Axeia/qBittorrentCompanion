023cd11ccbdba85c3f8059a6e3c28d077192f863
Pascal Bakhuis <pbakhuis@gmail.com>
2024-11-10 22:16:15 +0000

    Extending string class with replace first and last options

---
+++ qBittorrentCompanion/Extensions/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static string ReplaceFirstOccurrence(this string source, string oldSubstring, string newSubstring)
+    {
+        return ReplaceOccurrence(source, oldSubstring, newSubstring, false);
+    }
+
+    /// <summary>
+    /// Replace the last occurence of oldSubstring with newSubstring
+    /// If there's no occurence of oldSubstring it simply returns the original string.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="oldSubstring"></param>
+    /// <param name="newSubstring"></param>
+    /// <returns></returns>
+    public static string ReplaceLastOccurrence(this string source, string oldSubstring, string newSubstring)
+    {
+        return ReplaceOccurrence(source, oldSubstring, newSubstring, true);
+    }
+
+    /// <summary>
+    /// Replace the last occurence of oldSubstring with newSubstring
+    /// If there's no occurence of oldSubstring it simply returns the original string.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="oldSubstring"></param>
+    /// <param name="newSubstring"></param>
+    /// <returns></returns>
+    private static string ReplaceOccurrence(string source, string oldSubstring, string newSubstring, bool replaceLast)
+    {
+        int place = replaceLast ? source.LastIndexOf(oldSubstring) : source.IndexOf(oldSubstring);
+
+        return place == -1
+            ? source
+            : source.Remove(place, oldSubstring.Length).Insert(place, newSubstring);
+    }
+}
