7b4ab15949f10c9dd81ad5e439c3506f0d91977e
Pascal Bakhuis <pbakhuis@gmail.com>
2024-11-10 23:13:16 +0000

    Improved documentation

--- qBittorrentCompanion/Extensions/StringExtensions.cs
+++ qBittorrentCompanion/Extensions/StringExtensions.cs
@@ -2,6 +2,15 @@
 
 public static class StringExtensions
 {
+
+    /// <summary>
+    /// Replace the last occurence of oldSubstring with newSubstring
+    /// If there's no occurence of oldSubstring it simply returns the original string.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="oldSubstring"></param>
+    /// <param name="newSubstring"></param>
+    /// <returns></returns>
     public static string ReplaceFirstOccurrence(this string source, string oldSubstring, string newSubstring)
     {
         return ReplaceOccurrence(source, oldSubstring, newSubstring, false);
@@ -21,12 +30,14 @@ public static class StringExtensions
     }
 
     /// <summary>
-    /// Replace the last occurence of oldSubstring with newSubstring
-    /// If there's no occurence of oldSubstring it simply returns the original string.
+    /// Just use <see cref="ReplaceFirstOccurrence(string, string, string)">ReplaceFirstOccurrence</see> 
+    /// or <see cref="ReplaceLastOccurrence(string, string, string)">ReplaceLastOccurrence</see>
+    /// they're more clear in intent, this just houses the shared logic.
     /// </summary>
     /// <param name="source"></param>
     /// <param name="oldSubstring"></param>
     /// <param name="newSubstring"></param>
+    /// <param name="replaceLast"></param>
     /// <returns></returns>
     private static string ReplaceOccurrence(string source, string oldSubstring, string newSubstring, bool replaceLast)
     {
