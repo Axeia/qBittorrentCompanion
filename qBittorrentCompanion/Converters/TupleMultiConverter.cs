5a4d7db9365a0864e8afb60e44ed12491402a9a5
Pascal Bakhuis <pbakhuis@gmail.com>
2024-12-20 00:53:22 +0000

    Convert to allow sending a tuple as CommandParameter

---
+++ qBittorrentCompanion/Converters/TupleMultiConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace qBittorrentCompanion.Converters
+{
+    internal class TupleMultiConverter : IMultiValueConverter
+    {
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return (values[0]?.ToString(), values[1]?.ToString());
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
