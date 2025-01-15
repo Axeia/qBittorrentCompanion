52b9573e7a6f651265d8ec349b60a6c7bc595234
Pascal Bakhuis <pbakhuis@gmail.com>
2025-01-15 02:48:51 +0000

    Will serve as a base for RssAutoDownloadingRuleViewModel and MatchTestRowViewModel as they share functionality

---
+++ qBittorrentCompanion/ViewModels/RssRuleIsMatchViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace qBittorrentCompanion.ViewModels
+{
+    public class RssRuleIsMatchViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _isMatch = false;
+        public bool IsMatch
+        {
+            get => _isMatch;
+            set
+            {
+                if (_isMatch != value)
+                {
+                    _isMatch = value;
+                    OnPropertyChanged(nameof(IsMatch));
+                }
+            }
+        }
+
+        public static string WildCardToRegular(string value)
+        {
+            // Split the input string by white spaces and "|"
+            var parts = value.Split(new char[] { ' ', '\t', '\n', '\r', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Convert each part into a regex pattern
+            var patterns = parts.Select(part => Regex.Escape(part).Replace("\\?", ".").Replace("\\*", ".*"));
+
+            // Join the patterns with ".*", which functions as an AND operator in regex
+            var regexPattern = string.Join(".*", patterns);
+
+            return regexPattern;
+        }
+
+        public static bool IsTextMatch(string toMatch, string mustContain, string mustNotContain, string epFilter, bool isRegex)
+        {
+            if (mustContain == string.Empty && mustNotContain == string.Empty)
+                return false;
+
+            if (mustContain != string.Empty)
+            {
+                Regex mustContainRegex = isRegex
+                    ? new Regex(mustContain)
+                    : new Regex(WildCardToRegular(mustContain));
+
+                if (!mustContainRegex.Match(toMatch).Success)
+                    return false;
+            }
+
+            if (mustNotContain != string.Empty)
+            {
+                Regex mustNotContainRegex = isRegex
+                    ? new Regex(mustNotContain)
+                    : new Regex(WildCardToRegular(mustNotContain));
+
+                return !mustNotContainRegex.Match(toMatch).Success;
+            }
+
+            //TODO implement EpFilter
+
+
+            return true;
+        }
+    }
+}
