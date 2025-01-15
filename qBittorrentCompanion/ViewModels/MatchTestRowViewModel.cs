cb4d1c37de79501a52fa08b26bdc6c91ce8c3861
Pascal Bakhuis <pbakhuis@gmail.com>
2025-01-15 02:49:56 +0000

    Now enherits from RssRuleIsMatchViewModel which now contains IsMatch instead

--- qBittorrentCompanion/ViewModels/MatchTestRowViewModel.cs
+++ qBittorrentCompanion/ViewModels/MatchTestRowViewModel.cs
@@ -1,47 +1,19 @@
-﻿using Avalonia.Media;
-using DynamicData.Aggregation;
-using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reactive.Joins;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace qBittorrentCompanion.ViewModels
+﻿namespace qBittorrentCompanion.ViewModels
 {
-    public class MatchTestRowViewModel : ViewModelBase
+    public class MatchTestRowViewModel : RssRuleIsMatchViewModel
     {
-
-        private bool _isMatch = false;
-        public bool IsMatch
-        {
-            get { Debug.WriteLine($">{_isMatch}"); return _isMatch; }
-            set => this.RaiseAndSetIfChanged(ref _isMatch, value);
-        }
-
         private string _matchTest = "";
         public string MatchTest
         {
             get => _matchTest;
             set
             {
-                if ( value != _matchTest)
+                if (value != _matchTest)
                 {
-                    this.RaiseAndSetIfChanged(ref _matchTest, value);
+                    _matchTest = value;
+                    OnPropertyChanged(nameof(MatchTest));
                 }
             }
         }
-
-        public MatchTestRowViewModel(string text = "") 
-        {
-            //RegexStr = regex;
-        }
-
-
-
     }
 }
