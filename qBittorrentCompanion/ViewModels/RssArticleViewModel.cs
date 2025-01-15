55178569d618b7e7d3e5e6dc05c3a7bf6fa0b9ce
Pascal Bakhuis <pbakhuis@gmail.com>
2025-01-15 03:05:19 +0000

    Now enherits from RssRuleIsMatchViewModel allowing for the deletion of the functionality found in there.
    Also removed the temprorary IsMatch assignment in the constructor which was used for testing

--- qBittorrentCompanion/ViewModels/RssArticleViewModel.cs
+++ qBittorrentCompanion/ViewModels/RssArticleViewModel.cs
@@ -9,35 +9,13 @@ using System.Text.RegularExpressions;
 
 namespace qBittorrentCompanion.ViewModels
 {
-    public class RssArticleViewModel : INotifyPropertyChanged
+    public class RssArticleViewModel : RssRuleIsMatchViewModel
     {
         private readonly RssArticle _rssArticle;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
 
         public RssArticleViewModel(RssArticle rssArticle)
         {
             _rssArticle = rssArticle;
-            IsMatch = _rssArticle.Title.Contains("[");
-        }
-
-        private bool _isMatch = false;
-        public bool IsMatch
-        {
-            get => _isMatch;
-            set
-            {
-                if (_isMatch != value)
-                {
-                    _isMatch = value;
-                    OnPropertyChanged(nameof(IsMatch));
-                }
-            }
         }
 
         /// <summary>
@@ -84,50 +62,6 @@ namespace qBittorrentCompanion.ViewModels
         /// <inheritdoc cref="RssArticle.AdditionalData"/>
         /// </summary>
         public IDictionary<string, JToken> AdditionalData => _rssArticle.AdditionalData;
-
-        public static string WildCardToRegular(string value)
-        {
-            // Split the input string by white spaces and "|"
-            var parts = value.Split(new char[] { ' ', '\t', '\n', '\r', '|' }, StringSplitOptions.RemoveEmptyEntries);
-
-            // Convert each part into a regex pattern
-            var patterns = parts.Select(part => Regex.Escape(part).Replace("\\?", ".").Replace("\\*", ".*"));
-
-            // Join the patterns with ".*", which functions as an AND operator in regex
-            var regexPattern = string.Join(".*", patterns);
-
-            return regexPattern;
-        }
-
-        public static bool IsTextMatch(string toMatch, string mustContain, string mustNotContain, string epFilter, bool isRegex)
-        {
-            if (mustContain == string.Empty && mustNotContain == string.Empty)
-                return false;
-
-            if (mustContain != string.Empty)
-            {
-                Regex mustContainRegex = isRegex 
-                    ? new Regex(mustContain) 
-                    : new Regex(WildCardToRegular(mustContain));
-
-                if (!mustContainRegex.Match(toMatch).Success)
-                    return false;
-            }
-
-            if (mustNotContain != string.Empty)
-            {
-                Regex mustNotContainRegex = isRegex
-                    ? new Regex(mustNotContain)
-                    : new Regex(WildCardToRegular(mustNotContain));
-
-                return !mustNotContainRegex.Match(toMatch).Success;
-            }
-
-            //TODO implement EpFilter
-            
-
-            return true;
-        }
     }
 }
 
