6da8c610e12b22829f3239a726ccf2a637d3b8ca
Pascal Bakhuis <pbakhuis@gmail.com>
2025-01-23 16:31:48 +0000

    Extends IcoWindow and adds hotkey to close window by pressing escape
    Also focuses the first focusable element it can find as the HotKey doesn't work unless something in the window receives focus.

---
+++ qBittorrentCompanion/Views/EscIcoWindow.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Input;
+using Avalonia.VisualTree;
+using ReactiveUI;
+using System.Linq;
+using System.Reactive;
+
+namespace qBittorrentCompanion.Views
+{
+    /// <summary>
+    /// Extends IcoWindow and adds the option to close the window using the escape key
+    /// </summary>
+    public class EscIcoWindow : IcoWindow
+    {
+        public ReactiveCommand<Unit, Unit> CloseWindowCommand { get; }
+
+        public EscIcoWindow()
+        {
+            CloseWindowCommand = ReactiveCommand.Create(this.Close);
+
+            Loaded += EscIcoWindow_Loaded;
+        }
+
+        private void EscIcoWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var closeWindowKeyBinding = new KeyBinding
+            {
+                Gesture = new KeyGesture(Key.Escape, KeyModifiers.None),
+                Command = CloseWindowCommand
+            };
+            KeyBindings.Add(closeWindowKeyBinding);
+
+            // For whatever reason a control must have focus for the KeyBinding to work.
+            FocusFirstFocusableControl();
+        }
+
+        private void FocusFirstFocusableControl()
+        {
+            if (this.GetVisualDescendants().OfType<IInputElement>().Any(c => c.IsFocused))
+                return;
+
+            var focusableControl = this.GetVisualDescendants()
+                .OfType<IInputElement>()
+                .FirstOrDefault(c => c.IsEnabled && c.Focusable);
+
+            focusableControl?.Focus();
+        }
+    }
+}
