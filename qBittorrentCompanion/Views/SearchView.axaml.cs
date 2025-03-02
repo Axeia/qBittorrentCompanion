c8addb3179becbd20089e09057da442178e09fcc
Pascal Bakhuis <pbakhuis@gmail.com>
2025-03-02 04:39:59 +0000

    Changed to a 2 column layout
    Improves visual consistency between it and other tabs

--- qBittorrentCompanion/Views/SearchView.axaml.cs
+++ qBittorrentCompanion/Views/SearchView.axaml.cs
@@ -18,15 +18,6 @@ namespace qBittorrentCompanion.Views
             InitializeComponent();
             this.Loaded += SearchView_Loaded;
             this.DataContext = new SearchViewModel();
-
-            // Subscribe to resize actions
-            this.AttachedToVisualTree += (sender, e) =>
-            {
-                if (this.GetVisualRoot() is Window window)
-                {
-                    window.GetObservable(Window.BoundsProperty).Subscribe(Resize);
-                }
-            };
         }
 
         private void SearchView_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
@@ -36,12 +27,6 @@ namespace qBittorrentCompanion.Views
                 searchVm.Initialise();
                 SetBindings();
             }
-        }
-
-        // Callback function for resize actions
-        private void Resize(Rect size)
-        {
-            FiltersGrid.IsVisible = size.Width < 1220;
         }
 
         private void SearchPluginButton_Click(object? sender, RoutedEventArgs e)
