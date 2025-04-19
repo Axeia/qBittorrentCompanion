b2f74f95c5d43fa9965673d7839b7e40d6e70d63
Pascal Bakhuis <pbakhuis@gmail.com>
2025-04-19 22:35:42 +0100

    Changed how feeds/tags are selected

--- qBittorrentCompanion/Views/RssRuleView.axaml.cs
+++ qBittorrentCompanion/Views/RssRuleView.axaml.cs
@@ -4,16 +4,12 @@ using Avalonia.Threading;
 using Avalonia.VisualTree;
 using QBittorrent.Client;
 using qBittorrentCompanion.ViewModels;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Avalonia.Input;
-using AvaloniaEdit;
-using AvaloniaEdit.TextMate;
-using qBittorrentCompanion.AvaloniaEditor;
-using System.ComponentModel;
 using System;
+using Avalonia.Media;
+using qBittorrentCompanion.Helpers;
 
 namespace qBittorrentCompanion.Views
 {
@@ -29,6 +25,31 @@ namespace qBittorrentCompanion.Views
                     new RssAutoDownloadingRule(),
                     ""
                 );
+            }
+            Loaded += RssRuleView_Loaded;
+        }
+
+        private void RssRuleView_Loaded(object? sender, RoutedEventArgs e)
+        {
+        }
+
+        private void RemoveFeedButton_Click(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is RssAutoDownloadingRuleViewModel radRuleVm 
+                && sender is Button button 
+                && button.DataContext is RssFeedViewModel rfvm)
+            {
+                radRuleVm.SelectedFeeds.Remove(rfvm);
+            }
+        }
+
+        private void RemoveTagButton_Click(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is RssAutoDownloadingRuleViewModel radRuleVm
+                && sender is Button button
+                && button.DataContext is string tag)
+            {
+                radRuleVm.SelectedTags.Remove(tag);
             }
         }
 
@@ -46,6 +67,83 @@ namespace qBittorrentCompanion.Views
                 {
                     textBox.Text = textBox.Text.Trim();
                 });
+            }
+        }
+
+        private void AutoCompleteBox_GotFocus(object? sender, GotFocusEventArgs e)
+        {
+            if (sender is AutoCompleteBox acb)
+            {
+                acb.IsDropDownOpen = true;
+            }
+        }
+
+        private void MustContainTextEditor_GotFocus(object? sender, GotFocusEventArgs e)
+        {
+            MustContainBorder.BorderBrush = new SolidColorBrush(ThemeColors.SystemAccent);
+        }
+
+        private void CheckBox_Checked(object? sender, RoutedEventArgs e)
+        {
+            if(sender is CheckBox chkb 
+                && chkb.DataContext is RssFeedViewModel rfvm
+                && DataContext is RssAutoDownloadingRuleViewModel radrvm)
+            {
+                if(!radrvm.SelectedFeeds.Contains(rfvm))
+                    radrvm.SelectedFeeds.Add(rfvm);
+            }
+        }
+
+        private void CheckBox_Unchecked(object? sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox chkb
+                && chkb.DataContext is RssFeedViewModel rfvm
+                && DataContext is RssAutoDownloadingRuleViewModel radrvm)
+            {
+                radrvm.SelectedFeeds.Remove(rfvm);
+            }
+        }
+
+        /// <summary>
+        /// Adds to Selected list when the dropdown is closed (happens when an items is selected or enter is pressed)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AutoCompleteBox_DropDownClosed(object? sender, EventArgs e)
+        {
+            if (sender is AutoCompleteBox acb
+                && DataContext is RssAutoDownloadingRuleViewModel radrvm)
+            {
+                var listType = radrvm.RssFeeds.GetType().GetGenericArguments().FirstOrDefault();
+                if (listType == null)
+                {
+                    Debug.WriteLine("Unable to determine list type.");
+                    return;
+                }
+
+                // Use reflection to find the matching item and cast it
+                var matchingItem = radrvm.RssFeeds
+                    .FirstOrDefault(item => item.ToString().Equals(acb.Text, StringComparison.OrdinalIgnoreCase));
+
+                if (matchingItem != null && listType.IsInstanceOfType(matchingItem))
+                {
+                    if (!radrvm.SelectedFeeds.Contains(matchingItem))
+                    {
+                        radrvm.SelectedFeeds.Add(matchingItem);
+                        acb.Text = ""; 
+                        acb.IsDropDownOpen = true;
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn 
+                && DataContext is RssAutoDownloadingRuleViewModel radrvm
+                && btn.DataContext is RssFeedViewModel rfvm)
+            {
+                radrvm.SelectedFeeds.Remove(rfvm);
             }
         }
     }
