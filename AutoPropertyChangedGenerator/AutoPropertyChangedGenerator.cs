5300f970c3656531e6a365d01b6641ef80083326
Pascal Bakhuis <pbakhuis@gmail.com>
2025-07-28 02:30:52 +0100

    Added documentation descriptor

--- AutoPropertyChangedGenerator/AutoPropertyChangedGenerator.cs
+++ AutoPropertyChangedGenerator/AutoPropertyChangedGenerator.cs
@@ -1,5 +1,10 @@
 ﻿namespace AutoPropertyChangedGenerator
 {
+    /// <summary>
+    /// Generates a public property with ReactiveUI change notification for this field.
+    /// The property name will be the field name in PascalCase without underscores.
+    /// e.g. private int _myCounter; becomes public int MyCounter
+    /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public sealed class AutoPropertyChangedAttribute : Attribute
     {
