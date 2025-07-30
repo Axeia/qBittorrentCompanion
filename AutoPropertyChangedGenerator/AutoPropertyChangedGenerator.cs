6664498050d3aadd67c96f2a2854a9ef4c57ad3b
Pascal Bakhuis <pbakhuis@gmail.com>
2025-07-30 05:15:59 +0100

    Improved documentation of AutoProxyPropertyChangedAttribute, assigned default of null to name

--- AutoPropertyChangedGenerator/AutoPropertyChangedGenerator.cs
+++ AutoPropertyChangedGenerator/AutoPropertyChangedGenerator.cs
@@ -1,4 +1,6 @@
-﻿namespace AutoPropertyChangedGenerator
+﻿using System;
+
+namespace AutoPropertyChangedGenerator
 {
     /// <summary>
     /// Generates a public property with ReactiveUI change notification for this field.
@@ -10,11 +12,19 @@
     {
     }
 
-
+    /// <summary>
+    /// Generates a proxy property that forwards to a property on the annotated field.
+    /// <b>HIGHLY Recommended</b> to use nameof() for compile-time safety and easier refractoring
+    /// </summary>
+    /// <example>
+    /// [AutoProxyPropertyChanged(nameof(RssAutoDownloadingRule.SmartFilter))]
+    /// [AutoProxyPropertyChanged(nameof(RssAutoDownloadingRule.PreviouslyMatchedEpisodes), "Episodes")]
+    /// private RssAutoDownloadingRule _rule;
+    /// </example>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-    public sealed class AutoProxyPropertyChangedAttribute(string? property, string? name) : Attribute
+    public sealed class AutoProxyPropertyChangedAttribute(string propertyName, string? customName = null) : Attribute
     {
-        public string? Property { get; set; } = property;
-        public string? Name { get; set; } = name ?? property;
+        public string PropertyName { get; } = propertyName;
+        public string? CustomName { get; } = customName;
     }
 }
