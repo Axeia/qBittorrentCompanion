95dc8cdc503b34c782aa0e8a18646a81437f70f6
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-01 00:57:02 +0100

    Updated to use AutoPropertyChanged and AutoProxyPropertyChanged

--- qBittorrentCompanion/ViewModels/CategoryCountViewModel.cs
+++ qBittorrentCompanion/ViewModels/CategoryCountViewModel.cs
@@ -1,43 +1,14 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
+﻿using AutoPropertyChangedGenerator;
+using QBittorrent.Client;
+using ReactiveUI;
 
 namespace qBittorrentCompanion.ViewModels
 {
-    public class CategoryCountViewModel(QBittorrent.Client.Category category) : INotifyPropertyChanged
+    public partial class CategoryCountViewModel(Category category) : ReactiveObject
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-        private QBittorrent.Client.Category _category = category;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public string Name
-        {
-            get { return _category.Name; }
-            set
-            {
-                if (value != _category.Name)
-                {
-                    _category.Name = value;
-                    OnPropertyChanged(nameof(Name));
-                }
-            }
-        }
-        public string SavePath
-        {
-            get { return _category.SavePath; }
-            set
-            {
-                if (value != _category.SavePath)
-                {
-                    _category.SavePath = value;
-                    OnPropertyChanged(nameof(SavePath));
-                }
-            }
-        }
+        [AutoProxyPropertyChanged(nameof(Category.Name))]
+        [AutoProxyPropertyChanged(nameof(Category.SavePath))]
+        private readonly Category _category = category;
 
         public bool HasPath => _category != null && !string.IsNullOrEmpty(_category.SavePath);
 
@@ -46,19 +17,8 @@ namespace qBittorrentCompanion.ViewModels
             return SavePath ?? string.Empty;
         }
 
+        [AutoPropertyChanged]
         private int _count = 0;
-        public int Count
-        {
-            get => _count;
-            set
-            {
-                if (_count != value)
-                {
-                    _count = value;
-                    OnPropertyChanged(nameof(Count));
-                }
-            }
-        }
 
         public bool IsEditable { get; set; } = true;
     }
