103922892245aea73ec271986a75f30737459534
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-08 00:35:09 +0100

    Added IsVisible and IsData properties
    Changed properties that aren't expected to change to simpler readonly assignment

--- qBittorrentCompanion/Logging/LogMessage.cs
+++ qBittorrentCompanion/Logging/LogMessage.cs
@@ -5,17 +5,29 @@ using System;
 
 namespace qBittorrentCompanion.Logging
 {
-    public partial class LogMessage(LogLevel level, string source, string title = "", string message = "") : ReactiveObject
+    public partial class LogMessage(LogLevel level, string source, string title = "", string message = "", bool isData = false) 
+        : ReactiveObject
     {
+        private readonly DateTime _timestamp = DateTime.Now;
+        public DateTime Timestamp => _timestamp;
+        public string Time => _timestamp.ToString("HH:mm:ss");
+        
+        private readonly LogLevel _level = level;
+        public LogLevel Level => _level;
+
+        private readonly string _source = source;
+        public string Source => _source;
+
+        private readonly string _title = title;
+        public string Title => _title;
+
+        private readonly string _message = message;
+        public string Message => _message;
+
+        private readonly bool _isData = isData;
+        public bool IsData => _isData;
+
         [AutoPropertyChanged]
-        private DateTime _timestamp = DateTime.Now;
-        [AutoPropertyChanged]
-        private LogLevel _level = level;
-        [AutoPropertyChanged]
-        private string _source = source;
-        [AutoPropertyChanged]
-        private string _title = title;
-        [AutoPropertyChanged]
-        private string _message = message;
+        private bool _isVisible = true;
     }
 }
