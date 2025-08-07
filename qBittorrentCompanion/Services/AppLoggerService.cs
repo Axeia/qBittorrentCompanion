5e10659a4966f0f7600ebc2c962a2a6909beef4c
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-07 14:59:01 +0100

    Centralised Logger to aid in debugging
    Mostly meant to inform users what's going on in the app and perhaps leave a trail for when things go wrong with reproducible bugs

---
+++ qBittorrentCompanion/Services/AppLoggerService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using qBittorrentCompanion.Logging;
+using Splat;
+using System;
+
+namespace qBittorrentCompanion.Services
+{
+    public partial class AppLoggerService
+    {
+        public static event Action<LogMessage>? LogMessageAdded;
+
+        public static void AddLogMessage(LogLevel level, string source, string title, string message) => LogMessageAdded?.Invoke(new LogMessage(
+                level: level,
+                source: source,
+                title: title,
+                message: message
+            ));
+
+        public static void AddLogMessage(LogLevel level, string source, string title, object message) =>
+            LogMessageAdded?.Invoke(new LogMessage(
+                level: level,
+                source: source,
+                title: title,
+                message: JsonConvert.SerializeObject(message, Formatting.Indented,
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        DefaultValueHandling = DefaultValueHandling.Ignore,
+                        NullValueHandling = NullValueHandling.Ignore
+                    })
+            ));
+
+    }
+}
