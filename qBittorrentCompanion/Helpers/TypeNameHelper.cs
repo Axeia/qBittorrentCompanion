8b1276b826b264e79b95a0ad2050846f64feb1ce
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-08 01:58:09 +0100

    Added a way to get the classname including namespace easily

---
+++ qBittorrentCompanion/Helpers/TypeNameHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace qBittorrentCompanion.Helpers
+{
+    public static class TypeNameHelper
+    {
+        public static string GetFullTypeName<T>() => typeof(T).FullName ?? typeof(T).Name;
+    }
+}
