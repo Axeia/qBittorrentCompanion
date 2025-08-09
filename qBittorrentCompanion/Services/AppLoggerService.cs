6f2063dfd366a8f93cd9a9a995c97a00b12358c0
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-09 15:38:03 +0100

    Added documentation, removed unused method

--- qBittorrentCompanion/Services/AppLoggerService.cs
+++ qBittorrentCompanion/Services/AppLoggerService.cs
@@ -9,6 +9,15 @@ namespace qBittorrentCompanion.Services
     {
         public static event Action<LogMessage>? LogMessageAdded;
 
+        /// <summary>
+        /// Adds a log message with the specified details and raises the <see cref="LogMessageAdded"/> event.
+        /// </summary>
+        /// <param name="level">The severity level of the log message.</param>
+        /// <param name="source">The source or origin of the log message, typically identifying the component or module. Consider using using <see cref="GetFullTypeName{T}"/></param>
+        /// <param name="title">The primary title or summary of the log message.</param>
+        /// <param name="message">The detailed content of the log message.</param>
+        /// <param name="secondaryTitle">An optional secondary title, recommend to be a file name if a file is involved, 
+        /// or if a data class is processed use its name through <see cref="GetFullTypeName{T}"/> </param>
         public static void AddLogMessage(LogLevel level, string source, string title, string message, string secondaryTitle = "") 
             => LogMessageAdded?.Invoke(new LogMessage(
                 level: level,
@@ -18,6 +27,7 @@ namespace qBittorrentCompanion.Services
                 secondaryTitle: secondaryTitle
             ));
 
+        ///<inheritdoc cref="AddLogMessage(LogLevel, string, string, string, string)"/>
         public static void AddLogMessage(LogLevel level, string source, string title, object message, string secondaryTitle = "") 
             => LogMessageAdded?.Invoke(new LogMessage(
                 level: level,
@@ -32,15 +42,5 @@ namespace qBittorrentCompanion.Services
                     }),
                 secondaryTitle: secondaryTitle
             ));
-
-        /// <summary>
-        /// Returns class including namespace prefix as a string
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public static string NSC<T>()
-        {
-            return typeof(T).FullName ?? typeof(T).Name;
-        }
     }
 }
