878924577cc1cfc87392907461f21900f700a6b2
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-12 03:59:21 +0100

    Converts an int to a 3 long padded string,
    * Values exceeding 99 are returned as 99+
    * The padding is done with spaces
    * The parameter "right" can be specified to pad from the right

---
+++ qBittorrentCompanion/Converters/FixedLengthIntConverter.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace qBittorrentCompanion.Converters
+{
+    public class FixedLengthIntConverter : IValueConverter
+    {
+        public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is int v)
+            {
+                if (parameter is string whereToPad && whereToPad == "Right")
+                {
+                    return v > 99
+                        ? "99+"
+                        : v.ToString().PadRight(3, ' ');
+                }
+                else
+                {
+                    return v > 99
+                        ? "99+"
+                        : v.ToString().PadLeft(3, ' ');
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            return BindingOperations.DoNothing;
+        }
+
+    }
+}
