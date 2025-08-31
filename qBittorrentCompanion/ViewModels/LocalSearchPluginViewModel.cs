53d06d058cd3d9284c83cca0f087b833a482d720
Pascal Bakhuis <pbakhuis@gmail.com>
2025-08-31 03:06:02 +0100

    Renamed SearchPluginViewModel to Remote, made Local enherit from it
    Allows for assigning a filename to LocalPluginViewModel

---
+++ qBittorrentCompanion/ViewModels/LocalSearchPluginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AutoPropertyChangedGenerator;
+using QBittorrent.Client;
+using ReactiveUI;
+using System;
+
+namespace qBittorrentCompanion.ViewModels
+{
+    /// <summary>
+    /// LocalSearchPluginViewModel only differs from RemoteSearchPluginViewModel in two aspects.
+    /// 1) It has a FileName property
+    /// 2) Its version can be set after the fact
+    /// <param name="searchPlugin"></param>
+    /// <param name="fileName"></param>
+    public partial class LocalSearchPluginViewModel(SearchPlugin searchPlugin, string fileName) : RemoteSearchPluginViewModel(searchPlugin)
+    {
+        [AutoPropertyChanged]
+        private string _fileName = fileName;
+
+        public new Version Version
+        {
+            get => _searchPlugin.Version;
+            set
+            {
+                if (value != _searchPlugin.Version)
+                {
+                    _searchPlugin.Version = value;
+                    this.RaisePropertyChanged(nameof(Version));
+                }
+            }
+        }
+    }
+}
---
+++ qBittorrentCompanion/ViewModels/RemoteSearchPluginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using AutoPropertyChangedGenerator;
+using QBittorrent.Client;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+
+namespace qBittorrentCompanion.ViewModels
+{
+    public partial class RemoteSearchPluginViewModel(SearchPlugin searchPlugin) : ReactiveObject
+    {
+        [AutoProxyPropertyChanged(nameof(SearchPlugin.IsEnabled))]
+        protected readonly SearchPlugin _searchPlugin = searchPlugin;
+        public SearchPlugin SearchPlugin => _searchPlugin;
+
+        public string FullName 
+            => _searchPlugin.FullName;
+        public string Name 
+            => _searchPlugin.Name;
+        public IReadOnlyList<SearchPluginCategory> Categories 
+            => _searchPlugin.Categories;
+        public Uri Url 
+            => _searchPlugin.Url;
+        public string UrlShortened
+            => _searchPlugin.Url == null ? string.Empty : _searchPlugin.Url.Host.StartsWith("www.", StringComparison.OrdinalIgnoreCase)
+                ? _searchPlugin.Url.Host[4..]
+                : _searchPlugin.Url.Host;
+        public Version Version 
+            => _searchPlugin.Version;
+    }
+}
