5dfc2d172446efc54b7d1d230ce86867de6c5069
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-01 02:53:02 +0100

    Added IsEnabled to replace base implementation
    * Writes to the config file when disabled. Previously done by LocalSearchPluginsViewModel. Now responsible for its own data
    * Improved/updated documentation

--- qBittorrentCompanion/ViewModels/LocalSearchPluginViewModel.cs
+++ qBittorrentCompanion/ViewModels/LocalSearchPluginViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using AutoPropertyChangedGenerator;
 using QBittorrent.Client;
+using qBittorrentCompanion.Services;
 using ReactiveUI;
 using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace qBittorrentCompanion.ViewModels
 {
     /// <summary>
-    /// LocalSearchPluginViewModel only differs from RemoteSearchPluginViewModel in two aspects.
+    /// LocalSearchPluginViewModel only differs from RemoteSearchPluginViewModel in three aspects.
     /// 1) It has a FileName property
-    /// 2) Its version can be set after the fact
+    /// 2) Its version can be set after the fact.
+    ///   This is because <see cref="PythonSearchBridge "/> cannot parse versions and they're set in separate step
+    /// 3) IsEnabled writes to <see cref="AppConfig"/>
     /// <param name="searchPlugin"></param>
     /// <param name="fileName"></param>
     public partial class LocalSearchPluginViewModel(SearchPlugin searchPlugin, string fileName) : RemoteSearchPluginViewModel(searchPlugin)
@@ -25,6 +30,38 @@ namespace qBittorrentCompanion.ViewModels
                 {
                     _searchPlugin.Version = value;
                     this.RaisePropertyChanged(nameof(Version));
+                }
+            }
+        }
+
+        public new bool IsEnabled
+        {
+            get => _searchPlugin.IsEnabled;
+            set
+            {
+                if (value != _searchPlugin.IsEnabled)
+                {
+                    _searchPlugin.IsEnabled = value;
+
+                    // As list for LINQ
+                    var disabledLocalSearchPlugins = ConfigService.DisabledLocalSearchPlugins.ToList();
+
+                    if (value == true)
+                    {
+                        // Remove from disabled list
+                        if (disabledLocalSearchPlugins.Remove(FileName)) // Only re-assign if something was actually removed                            
+                            ConfigService.DisabledLocalSearchPlugins = [.. disabledLocalSearchPlugins];
+                    }
+                    else
+                    {
+                        if (!disabledLocalSearchPlugins.Contains(FileName))
+                        {
+                            disabledLocalSearchPlugins.Add(FileName);
+                            ConfigService.DisabledLocalSearchPlugins = [.. disabledLocalSearchPlugins];
+                        }
+                    }
+
+                    this.RaisePropertyChanged(nameof(IsEnabled));
                 }
             }
         }
