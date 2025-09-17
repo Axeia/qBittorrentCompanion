fa382e03a81029d3b937fc504f457f711288967d
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-17 05:54:51 +0100

    Some convenient extensions to save a Svg straight to a .ico

---
+++ qBittorrentCompanion/Extensions/SkSvgExtensions.cs
@@ -0,0 +1,20 @@
+﻿using qBittorrentCompanion.Helpers;
+using Svg.Skia;
+using System.IO;
+
+namespace qBittorrentCompanion.Extensions
+{
+    public static class SKSvgExtensions
+    { 
+        public static bool SaveAsIco(this SKSvg skSvg, Stream output)
+        {
+            return IcoHelper.ConvertToIcon(skSvg, output);
+        }
+
+        public static bool SaveAsIco(this SKSvg skSvg, string filePath)
+        {
+            using var fs = File.Create(filePath);
+            return IcoHelper.ConvertToIcon(skSvg, fs);
+        }
+    }
+}
