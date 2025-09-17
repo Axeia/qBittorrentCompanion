de6804ef450467cfcecce3e9b3b4d22bdb7f8241
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-17 05:57:54 +0100

    Definition of the colors used in the logo (and its defaults)

---
+++ qBittorrentCompanion/Models/LogoColorsRecord.cs
@@ -0,0 +1,30 @@
+﻿namespace qBittorrentCompanion.Models
+{
+    public record LogoColorsRecord(
+        string Q, 
+        string B, 
+        string C, 
+        string GradientCenter, 
+        string GradientFill, 
+        string GradientRim
+    )
+    {
+        public static LogoColorsRecord LightDefault => new(
+            Q: "#fff",
+            B: "#fff",
+            C: "#fff",
+            GradientCenter: "maroon",
+            GradientFill: "red",
+            GradientRim: "maroon"
+        );
+
+        public static LogoColorsRecord DarkDefault => new(
+            Q: "#000",
+            B: "#000",
+            C: "#000",
+            GradientCenter: "maroon",
+            GradientFill: "red",
+            GradientRim: "maroon"
+        );
+    }
+}
