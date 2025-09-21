77b079f15bdec6aae78e64e8eac2cafe0709597a
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-21 02:17:37 +0100

    Added a background animation and removed unused code

--- qBittorrentCompanion/Views/LocalSettings/LocalSettingsWindow.axaml.cs
+++ qBittorrentCompanion/Views/LocalSettings/LocalSettingsWindow.axaml.cs
@@ -1,4 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Styling;
+using Avalonia.Threading;
 using qBittorrentCompanion.ViewModels.LocalSettings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace qBittorrentCompanion.Views
 {
@@ -19,7 +27,31 @@ namespace qBittorrentCompanion.Views
             //MatchColorPickersToCanvas();
             _localSettingsWindowViewModel = new LocalSettingsWindowViewModel();
             this.DataContext = _localSettingsWindowViewModel;
+            Loaded += LocalSettingsWindow_Loaded;
             //PreviewBgColorPicker.Color = Avalonia.Media.Colors.Transparent;
+        }
+
+        private void LocalSettingsWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var canvas = TabStripBackgroundCanvas;
+            var svgs = canvas.Children.OfType<Avalonia.Svg.Skia.Svg>();
+            foreach (var svg in svgs)
+            {
+                if (svg.Tag is string tagValue && int.TryParse(tagValue, out int delaySeconds))
+                {
+                    // Start each path's animation after its delay
+                    _ = Task.Run(async () =>
+                    {
+                        await Task.Delay(delaySeconds * 1000); // Convert to milliseconds
+
+                        // Add the class on UI thread to trigger animation
+                        _ = Dispatcher.UIThread.InvokeAsync(() =>
+                        {
+                            svg.Classes.Add("SpinLogo");
+                        });
+                    });
+                }
+            }
         }
     }
 }
