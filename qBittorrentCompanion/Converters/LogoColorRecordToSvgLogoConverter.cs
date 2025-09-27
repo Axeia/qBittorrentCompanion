44d1cd065355f24f62783ed04389aaba2464c0d7
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-27 01:20:12 +0100

    Given a `LogoColorsRecord` it returns a logo as SVG (string, with the provided colors)

---
+++ qBittorrentCompanion/Converters/LogoColorRecordToSvgLogoConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using qBittorrentCompanion.Helpers;
+using qBittorrentCompanion.Models;
+using System;
+using System.Globalization;
+
+namespace qBittorrentCompanion.Converters
+{
+    public class LogoColorRecordToSvgLogoConverter : IValueConverter
+    {
+        public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is LogoColorsRecord lcr)
+                return LogoHelper.GetLogoAsXDocument(lcr).ToString();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
+        {
+            return BindingOperations.DoNothing;
+        }
+
+    }
+}
