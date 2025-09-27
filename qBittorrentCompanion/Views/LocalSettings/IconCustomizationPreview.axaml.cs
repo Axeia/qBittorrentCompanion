bccd3cee35f82b933df3c9aa763a09bbc0954821
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-27 01:22:37 +0100

    Removed unused code

--- qBittorrentCompanion/Views/LocalSettings/IconCustomizationPreview.axaml.cs
+++ qBittorrentCompanion/Views/LocalSettings/IconCustomizationPreview.axaml.cs
@@ -1,13 +1,9 @@
 
 using Avalonia;
-using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Media;
 using Avalonia.Styling;
-using Avalonia.Themes.Fluent;
 using Avalonia.VisualTree;
-using qBittorrentCompanion.Helpers;
 using qBittorrentCompanion.Models;
 using qBittorrentCompanion.ViewModels.LocalSettings;
 using System;
@@ -67,26 +63,6 @@ namespace qBittorrentCompanion.Views.LocalSettings
             resolvedTheme ??= Application.Current?.ActualThemeVariant;
 
             return resolvedTheme == ThemeVariant.Dark;
-        }
-
-        private void GenerateAccentColorIconMenuItem_Click(object? sender, RoutedEventArgs e)
-        {
-            if (DataContext is IconCustomizationViewModel icvm)
-            {
-                //icvm.Q_Color = ThemeColors.SystemBaseHigh;
-                //icvm.B_Color = ThemeColors.SystemBaseHigh;
-                //icvm.C_Color = ThemeColors.SystemBaseHigh;
-                //icvm.GradientCenterColor = ThemeColors.SystemAccentLight3;
-                //icvm.GradientFillColor = ThemeColors.SystemAccentDark1;
-                //icvm.GradientRimColor = ThemeColors.SystemAccentDark3;
-
-                icvm.Q_Color = IsInDarkMode ? ThemeColors.SystemAccentLight3 : ThemeColors.SystemAccentDark3;
-                icvm.B_Color = IsInDarkMode ? ThemeColors.SystemAccentLight3 : ThemeColors.SystemAccentDark3;
-                icvm.C_Color = IsInDarkMode ? ThemeColors.SystemAccentLight3 : ThemeColors.SystemAccentDark3;
-                icvm.GradientCenterColor = Colors.Transparent;
-                icvm.GradientFillColor = Colors.Transparent;
-                icvm.GradientRimColor = Colors.Transparent;
-            }
         }
     }
 }
