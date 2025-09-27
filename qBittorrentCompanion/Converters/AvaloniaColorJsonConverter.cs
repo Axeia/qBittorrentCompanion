8acc293c86bcdb97bd80240c88180cf3240980d4
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-27 17:38:16 +0100

    JsonConverter allowing efficient converting of Avalonia.Media.Color to string (and back)

---
+++ qBittorrentCompanion/Converters/AvaloniaColorJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Media;
+using Newtonsoft.Json;
+using System;
+
+namespace qBittorrentCompanion.Converters
+{
+    public class AvaloniaColorJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+            => objectType == typeof(Color);
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            var color = (Color)value!;
+            writer.WriteValue(color.ToString()); 
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            var colorString = (string?)reader.Value ?? "#000000";
+            return Color.Parse(colorString);
+        }
+    }
+}
