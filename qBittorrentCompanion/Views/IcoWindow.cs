8f0c0610977c44f027ec4b0a2768f160dd155dc3
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-27 06:46:41 +0100

    Changed to use `App` better

--- qBittorrentCompanion/Views/IcoWindow.cs
+++ qBittorrentCompanion/Views/IcoWindow.cs
@@ -8,28 +8,35 @@ namespace qBittorrentCompanion.Views
     {
         public IcoWindow()
         {
-            // Update window icon if the dark/light mode icons are updated
-            this.Bind(IconProperty, new Binding
-            {
-                Source = App.Current,
-                Path = nameof(App.DarkModeWindowIcon),
-                Mode = BindingMode.OneWay
-            });
-            this.Bind(IconProperty, new Binding
-            {
-                Source = App.Current,
-                Path = nameof(App.LightModeWindowIcon),
-                Mode = BindingMode.OneWay
-            });
+            if(!Design.IsDesignMode)
+            { 
+                // Update window icon if the dark/light mode icons are updated
+                this.Bind(IconProperty, new Binding
+                {
+                    Source = App.Current,
+                    Path = nameof(App.DarkModeWindowIcon),
+                    Mode = BindingMode.OneWay
+                });
+                this.Bind(IconProperty, new Binding
+                {
+                    Source = App.Current,
+                    Path = nameof(App.LightModeWindowIcon),
+                    Mode = BindingMode.OneWay
+                });
 
-            // Update window icon if the theme is changed
-            ActualThemeVariantChanged += (_, args) =>
-            {
-                if(App.Current is App app)
-                this.Icon = ActualThemeVariant == ThemeVariant.Dark
-                    ? app.DarkModeWindowIcon
-                    : app.LightModeWindowIcon;
-            };
+                this.Icon = App.Current?.CurrentModeWindowIcon;
+
+                // Update window icon if the theme is changed
+                ActualThemeVariantChanged += (_, args) =>
+                {
+                    if (App.Current is App app)
+                    {
+                        app.CurrentModeWindowIcon = ActualThemeVariant == ThemeVariant.Dark
+                            ? app.DarkModeWindowIcon
+                            : app.LightModeWindowIcon;
+                    }
+                };
+            }
         }
     }
 }
