3851ee449016f495bf2053f274f97089a1c94ec4
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-28 14:17:01 +0100

    Added theme for ColorPicker as just a checkbox

---
+++ qBittorrentCompanion/ViewModels/LocalSettings/DirectoriesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AutoPropertyChangedGenerator;
+
+namespace qBittorrentCompanion.ViewModels.LocalSettings
+{
+    public partial class DirectoriesViewModel() : ViewModelBase
+    {
+
+        [AutoPropertyChanged]
+        private string _customName = string.Empty;
+
+    }
+}
