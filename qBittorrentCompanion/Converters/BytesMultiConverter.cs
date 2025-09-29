856243d2dc81c5d3e2ff18597f8978848edf6764
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-29 10:57:05 +0100

    Improved documentation & code

--- qBittorrentCompanion/Converters/BytesMultiConverter.cs
+++ qBittorrentCompanion/Converters/BytesMultiConverter.cs
@@ -1,14 +1,32 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
 using qBittorrentCompanion.Helpers;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
+using static qBittorrentCompanion.Helpers.DataConverter;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a numeric byte value and <see cref="DataConverter.ByteUnit"/> into a scaled double value.
+    /// </summary>
     public class BytesMultiConverter : IMultiValueConverter
     {
+        /// <summary>
+        /// Converts a numeric value and a unit string into a scaled double value.
+        /// Expects <paramref name="values"/> to contain:
+        /// <list type="bullet">
+        /// <item><description>Index 0: an <see cref="int"/>, <see cref="long"/>, or <see cref="Int64"/> representing the byte count</description></item>
+        /// <item><description>Index 1: a <see cref="string"/> or <see cref="ByteUnit"/> representing the unit (e.g. "MiB", "GiB"). Must match a value in <see cref="ByteUnit"/>.</description></item>
+        /// </list>
+        /// Returns the byte count divided by the multiplier for the unit, as a <see cref="double"/>.
+        /// </summary>
+        /// <param name="values">[0] => numeric (int, long, Int64). [1] => <see cref="DataConverter.ByteUnit"/></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
             long? valToConvert = null;
@@ -16,24 +34,15 @@ namespace qBittorrentCompanion.Converters
             if (values is null)
                 return null;
 
-            if (values[0] is int intVal)
-                valToConvert = intVal;
-            if (values[0] is long longVal)
-                valToConvert = longVal;
-            if (values[0] is Int64 int64Val)
-                valToConvert = int64Val;
+            if (values[0] is IConvertible convertible)
+                valToConvert = convertible.ToInt64(culture);
 
-            if (valToConvert != null && values[1] is string sizeUnit)
+            if (valToConvert != null && values[1] is ByteUnit sizeUnit)
             {
-                //Debug.WriteLine($"(using: {sizeUnit}) {valToConvert} / {DataConverter.GetMultiplierForUnit(sizeUnit)} = {valToConvert / DataConverter.GetMultiplierForUnit(sizeUnit)}");
-                return System.Convert.ToDouble(valToConvert / DataConverter.GetMultiplierForUnit(sizeUnit));
+                return System.Convert.ToDouble(valToConvert / DataConverter.Multipliers[sizeUnit]);
             }
-            /*else
-            {
-                Debug.WriteLine($"BytesConverter does not deal with values of type {(values[0] == null ? "null" : values[0].GetType())} or parameters of type {(parameter == null ? "null" : parameter.GetType())}");
-            }*/
 
-            return null;
+            return BindingOperations.DoNothing;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
