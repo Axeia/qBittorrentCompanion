e4e48e8ca1d3cb36c641d20361889fe5946f2e02
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-29 10:56:29 +0100

    Implemented ConvertBack, Removed empty lines, improved documentation

--- qBittorrentCompanion/Converters/BrushToHexConverter.cs
+++ qBittorrentCompanion/Converters/BrushToHexConverter.cs
@@ -1,16 +1,16 @@
-﻿using Avalonia.Data;
-using Avalonia.Data.Converters;
+﻿using Avalonia.Data.Converters;
 using Avalonia.Media;
-using qBittorrentCompanion.Helpers;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Takes a <see cref="ISolidColorBrush"/>, extracts its <seealso cref="Color"/> and converts to a 9 character string<br/>.
+    /// Or the other way around, parses a string to a color by usings its <seealso cref="Color.Parse(string)"/> 
+    /// like #FF00FF00 for a fully opaque 
+    /// </summary>
     public class BrushToHexConverter : IValueConverter
     {
         public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
@@ -18,15 +18,27 @@ namespace qBittorrentCompanion.Converters
             if (value is ISolidColorBrush solidColorBrush)
             {
                 var color = solidColorBrush.Color;
+
+                // In this project `return color.ToString(ColorFormat.HEX_ARGB);` would work as well,
+                // but this reduces dependencies
                 return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
             }
+
             return "#??????";
         }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
-            return BindingOperations.DoNothing;
+            /// Untested, just added for completeness sake
+            try
+            {
+                return value is string str ? Color.Parse(str) : Colors.Fuchsia;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                return Colors.Aqua;
+            }
         }
-
     }
 }
