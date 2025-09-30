b4d040805860da80bf248d069892a4a4497f27ec
Pascal Bakhuis <pbakhuis@gmail.com>
2025-10-01 00:26:00 +0100

    Added documentation, improved code readability

--- qBittorrentCompanion/Converters/SearchInOptionsConverter.cs
+++ qBittorrentCompanion/Converters/SearchInOptionsConverter.cs
@@ -7,36 +7,53 @@ using System.Globalization;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a <see cref="SearchInOption"/> enum value into a user-friendly description string,
+    /// and vice versa. Used for UI bindings where search scope is displayed or selected.
+    /// </summary>
+    /// <remarks>
+    /// Descriptions are centralized in <see cref="DataConverter.SearchInOptionDescriptions"/>.
+    /// This converter supports both forward and reverse mapping.
+    /// </remarks>
     public class SearchInOptionsConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="SearchInOption"/> to its description string.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="SearchInOption"/>.</param>
+        /// <returns>A description string for the search option.</returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is SearchInOption searchInOption)
-            {
-                return searchInOption switch
+            return value is SearchInOption option
+                ? option switch
                 {
                     SearchInOption.NamePlusExtension => DataConverter.SearchInOptionDescriptions.NamePlusExtension,
                     SearchInOption.Name => DataConverter.SearchInOptionDescriptions.Name,
                     SearchInOption.Extension => DataConverter.SearchInOptionDescriptions.Extension,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-            }
-            return DataConverter.SearchInOptionDescriptions.NamePlusExtension;
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown SearchInOption")
+                }
+                : DataConverter.SearchInOptionDescriptions.NamePlusExtension;
         }
+
+        /// <summary>
+        /// Converts a description string back into a <see cref="SearchInOption"/>.
+        /// </summary>
+        /// <param name="value">Expected to be a description string.</param>
+        /// <returns>The corresponding <see cref="SearchInOption"/>, or throws if unknown.</returns>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
             if (value is string str)
             {
-                if (str == DataConverter.SearchInOptionDescriptions.NamePlusExtension)
-                    return SearchInOption.NamePlusExtension;
-                if (str == DataConverter.SearchInOptionDescriptions.Name)
-                    return SearchInOption.Name;
-                if (str == DataConverter.SearchInOptionDescriptions.Extension)
-                    return SearchInOption.Extension;
-                throw new ArgumentOutOfRangeException();
+                return str switch
+                {
+                    var s when s == DataConverter.SearchInOptionDescriptions.NamePlusExtension => SearchInOption.NamePlusExtension,
+                    var s when s == DataConverter.SearchInOptionDescriptions.Name => SearchInOption.Name,
+                    var s when s == DataConverter.SearchInOptionDescriptions.Extension => SearchInOption.Extension,
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown search option description")
+                };
             }
 
-            return BindingOperations.DoNothing; ;
+            return BindingOperations.DoNothing;
         }
     }
 }
