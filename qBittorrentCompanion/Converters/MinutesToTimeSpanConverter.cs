95b3c692cd82a5dc142b70c9b3886e340dc25626
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-30 23:57:04 +0100

    Cleaned up code, added documentation and ConvertBack functionality
    * Documentation added for everything
    * ConvertBack added (although currently not used, so rather untested)
    * Removed unused `using`
    * Improved readability of code

--- qBittorrentCompanion/Converters/MinutesToTimeSpanConverter.cs
+++ qBittorrentCompanion/Converters/MinutesToTimeSpanConverter.cs
@@ -1,38 +1,48 @@
 ﻿using Avalonia.Data;
 using Avalonia.Data.Converters;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts between a <see cref="TimeSpan"/> and its total minutes as a numeric value.
+    /// Used for UI bindings where durations are edited or displayed in minutes.
+    /// </summary>
+    /// <remarks>
+    /// Supports <c>int</c>, <c>long</c>, <c>double</c>, and <c>decimal</c> inputs for conversion.
+    /// Returns <see cref="BindingOperations.DoNothing"/> for unsupported types.
+    /// </remarks>
     public class MinutesToTimeSpanConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="TimeSpan"/> to its total minutes as a <c>double</c>.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="TimeSpan"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>Total minutes as <c>double</c>, or <c>null</c> if input is invalid.</returns>
         public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is TimeSpan timeSpan)
-            {
-                return timeSpan.TotalMinutes;
-            }
-
-            return null;
+            return value is TimeSpan timeSpan
+                ? timeSpan.TotalMinutes
+                : null;
         }
 
+        /// <summary>
+        /// Converts a numeric value (minutes) into a <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="value">Expected to be a numeric type: <c>int</c>, <c>long</c>, <c>double</c>, or <c>decimal</c>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>A <see cref="TimeSpan"/> representing the given number of minutes, or <see cref="BindingOperations.DoNothing"/> if input is invalid.</returns>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is int || value is decimal || value is long || value is double)
-            {
-                var timeInMinutes = System.Convert.ToInt64(value);
-                return TimeSpan.FromMinutes(timeInMinutes);
-            }
-            else
-            {
-                return BindingOperations.DoNothing;
-            }
+            return value is int or long or double or decimal
+                ? TimeSpan.FromMinutes(System.Convert.ToDouble(value))
+                : BindingOperations.DoNothing;
         }
     }
 }
