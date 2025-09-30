e0b41048a41e61309f3f60a084782be418de5cee
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-30 14:41:57 +0100

    Improved documentation

--- qBittorrentCompanion/Converters/EnumToBooleanConverter.cs
+++ qBittorrentCompanion/Converters/EnumToBooleanConverter.cs
@@ -1,32 +1,52 @@
 ﻿using Avalonia.Data;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
-using QBittorrent.Client;
-using qBittorrentCompanion.Helpers;
-using qBittorrentCompanion.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts between an enum value and a boolean for use in UI bindings.<br/>
+    /// Typically used to bind the <c>.IsChecked</c> state of a control to an enum selection.<br/>
+    /// <br/>
+    /// Need to bind to multiple values? <see cref="EnumToBooleanMultiConverter"/>
+    /// </summary>
+    /// <remarks>
+    /// Basically the intended use is to be combined with binding <c>.IsChecked</c> on:
+    /// <list type="bullet">
+    /// <item><seealso cref="Avalonia.Controls.CheckBox"/></item>
+    /// <item><seealso cref="Avalonia.Controls.RadioButton"/></item>
+    /// <item><seealso cref="Avalonia.Controls.ToggleSplitButton"/></item>
+    /// <item><seealso cref="Avalonia.Controls.ToggleSwitch"/></item>
+    /// </list>
+    /// </remarks>
     public class EnumToBooleanConverter : IValueConverter
     {
+        /// <summary>
+        /// Returns <c>true</c> if the enum value equals the provided parameter.
+        /// </summary>
+        /// <param name="value">The current enum value.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">The enum value to compare against.</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns><c>true</c> if <paramref name="value"/> equals <paramref name="parameter"/>; otherwise <c>false</c>.</returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            if (value is null || parameter is null)
-                return false;
-
-            return value.Equals(parameter);
+            return value is not null && parameter is not null && value.Equals(parameter);
         }
 
+        /// <summary>
+        /// Returns the enum value (from <paramref name="parameter"/>) if the bound boolean is <c>true</c>.
+        /// </summary>
+        /// <param name="value">The boolean value from the UI.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">The enum value to return if <paramref name="value"/> is <c>true</c>.</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>The enum value if <paramref name="value"/> is <c>true</c>; otherwise <see cref="BindingOperations.DoNothing"/>.</returns>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return value?.Equals(true) == true && parameter != null 
-                ? parameter 
+            return value?.Equals(true) == true && parameter is not null
+                ? parameter
                 : BindingOperations.DoNothing;
         }
     }
--- qBittorrentCompanion/Converters/EnumToBooleanMultiConverter.cs
+++ qBittorrentCompanion/Converters/EnumToBooleanMultiConverter.cs
@@ -1,26 +1,43 @@
-﻿using Avalonia.Data;
-using Avalonia.Data.Converters;
+﻿using Avalonia.Data.Converters;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Compares two bound values and returns <c>true</c> if they are equal.
+    /// Typically used to bind the <c>IsChecked</c> state of a control to an enum selection
+    /// when both the current value and the target value are bound separately.
+    /// </summary>
+    /// <remarks>
+    /// This is the multi-binding equivalent of <see cref="EnumToBooleanConverter"/>.
+    /// Useful in scenarios where the selected enum and the candidate enum are both bound as separate sources.
+    /// </remarks>
     public class EnumToBooleanMultiConverter : IMultiValueConverter
     {
+        /// <summary>
+        /// Compares two bound values and returns <c>true</c> if they are equal.
+        /// </summary>
+        /// <param name="values">A list of two values: [current enum value, candidate enum value]</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Optional parameter (ignored)</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns><c>true</c> if both values are non-null and equal; otherwise <c>false</c>.</returns>
         public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
-            if (values.Count != 2 || values[0] == null || values[1] == null)
+            if (values.Count != 2 || values[0] is null || values[1] is null)
                 return false;
 
-            //Debug.WriteLine($"value: {values[0]} == {values[1]}");
             return values[0]!.Equals(values[1]);
         }
 
+        /// <summary>
+        /// Not supported — throws <see cref="NotImplementedException"/>.
+        /// </summary>
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException("EnumToBooleanMultiConverter does not support ConvertBack.");
         }
     }
 }
