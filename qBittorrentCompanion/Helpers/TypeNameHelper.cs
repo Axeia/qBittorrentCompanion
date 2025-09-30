3b86102ddc3e49805804c7cc26fdb2d6eef120ca
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-30 03:11:31 +0100

    Added documentation

--- qBittorrentCompanion/Helpers/TypeNameHelper.cs
+++ qBittorrentCompanion/Helpers/TypeNameHelper.cs
@@ -2,6 +2,10 @@
 
 namespace qBittorrentCompanion.Helpers
 {
+    /// <summary>
+    /// Returns the full type name including namespace, or the simple name if <c>FullName</c> is null.
+    /// Useful for logging, diagnostics, and reflection.
+    /// </summary>
     public static class TypeNameHelper
     {
         public static string GetFullTypeName<T>() => GetFullTypeName(typeof(T));
