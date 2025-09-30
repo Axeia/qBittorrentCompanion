6b5957b1bbb77e87b73a654a694e059810cd9923
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-30 23:54:24 +0100

    Added documentation, made code easier to read

--- qBittorrentCompanion/Converters/LogoColorRecordToSvgLogoConverter.cs
+++ qBittorrentCompanion/Converters/LogoColorRecordToSvgLogoConverter.cs
@@ -7,20 +7,38 @@ using System.Globalization;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a <see cref="LogoColorsRecord"/> into an SVG string representation.
+    /// Used to render themed logos in the UI based on user-selected or system-defined colors.
+    /// </summary>
+    /// <remarks>
+    /// Delegates SVG generation to <see cref="LogoHelper.GetLogoAsXDocument(LogoColorsRecord)"/>.
+    /// Returns an empty string if input is null or of unexpected type.
+    /// </remarks>
     public class LogoColorRecordToSvgLogoConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="LogoColorsRecord"/> into an SVG string.<br/>
+        /// <seealso cref="LogoHelper.GetLogoAsXDocument(LogoColorsRecord)"/>
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="LogoColorsRecord"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>An SVG string representing the logo, or an empty string if input is invalid.</returns>
         public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is LogoColorsRecord lcr)
-                return LogoHelper.GetLogoAsXDocument(lcr).ToString();
-
-            return string.Empty;
+            return value is LogoColorsRecord lcr
+                ? LogoHelper.GetLogoAsXDocument(lcr).ToString()
+                : string.Empty;
         }
 
+        /// <summary>
+        /// Not supported — returns <see cref="BindingOperations.DoNothing"/>.
+        /// </summary>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
             return BindingOperations.DoNothing;
         }
-
     }
 }
