756646110d70259e1c3c2e9e4c76371bb7e07402
Pascal Bakhuis <pbakhuis@gmail.com>
2025-10-01 00:00:44 +0100

    Added documentation, made code easier to read

--- qBittorrentCompanion/Converters/NegativeOneToNAConverter.cs
+++ qBittorrentCompanion/Converters/NegativeOneToNAConverter.cs
@@ -1,39 +1,54 @@
 ﻿using Avalonia.Data;
 using Avalonia.Data.Converters;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a sentinel value of <c>-1</c> (as <see cref="long"/>) into the string <c>"N/A"</c> for display.
+    /// Converts <c>"N/A"</c> back into <c>-1</c>, and parses other numeric strings into <see cref="long"/>.
+    /// </summary>
+    /// <remarks>
+    /// Useful for rendering optional or unavailable numeric values in a user-friendly way.
+    /// </remarks>
     public class NegativeOneToNAConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts <c>-1</c> to <c>"N/A"</c>, or returns the string representation of the value.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="long"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns><c>"N/A"</c> if value is <c>-1</c>, otherwise <c>value.ToString()</c>.</returns>
         public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is long && (long)value == -1)
-                return "N/A";
-            else
-                return value?.ToString() ?? "";
+            return value is long l && l == -1
+                ? "N/A"
+                : value?.ToString() ?? string.Empty;
         }
 
+        /// <summary>
+        /// Converts <c>"N/A"</c> to <c>-1</c>, or parses numeric strings into <see cref="long"/>.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="string"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns><c>-1</c> for <c>"N/A"</c>, parsed <see cref="long"/> for numeric strings, or <see cref="BindingOperations.DoNothing"/> otherwise.</returns>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is string stringValue && stringValue == "N/A")
+            if (value is string str)
             {
-                return -1;
-            }
-            else if (value is string numberString && long.TryParse(numberString, out long numberValue))
-            {
-                return numberValue;
-            }
-            else
-            {
-                return BindingOperations.DoNothing;
-            }
-        }
+                if (str == "N/A")
+                    return -1;
 
+                if (long.TryParse(str, out var number))
+                    return number;
+            }
+
+            return BindingOperations.DoNothing;
+        }
     }
 }
