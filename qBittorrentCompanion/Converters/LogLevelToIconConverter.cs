34f68bda1641cf54595a083e350ffe89cd1359df
Pascal Bakhuis <pbakhuis@gmail.com>
2025-09-30 16:54:39 +0100

    Added documentation

--- qBittorrentCompanion/Converters/LogLevelToIconConverter.cs
+++ qBittorrentCompanion/Converters/LogLevelToIconConverter.cs
@@ -7,13 +7,36 @@ using System.Globalization;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a <see cref="LogLevel"/> into a corresponding <see cref="Symbol"/> icon.
+    /// Used to visually represent log severity in the UI.
+    /// </summary>
+    /// <remarks>
+    /// Maps log levels to FluentIcons:
+    /// <list type="bullet">
+    /// <item><c>Debug</c> → <see cref="Symbol.Bug"/></item>
+    /// <item><c>Info</c> → <see cref="Symbol.Info"/></item>
+    /// <item><c>Warn</c> → <see cref="Symbol.Warning"/></item>
+    /// <item><c>Error</c> → <see cref="Symbol.ErrorCircle"/></item>
+    /// <item><c>Fatal</c> → <see cref="Symbol.ShieldError"/></item>
+    /// </list>
+    /// Falls back to <see cref="Symbol.QuestionCircle"/> for unknown or null values.
+    /// </remarks>
     public class LogLevelToIconConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="LogLevel"/> to a <see cref="Symbol"/> icon.<br/>
+        /// Falls back to <see cref="Symbol.QuestionCircle"/> for unknown values.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="LogLevel"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>A <see cref="Symbol"/> representing the log level.</returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            if (value is LogLevel logLevel)
-            {
-                return logLevel switch
+            return value is LogLevel logLevel
+                ? logLevel switch
                 {
                     LogLevel.Debug => Symbol.Bug,
                     LogLevel.Info => Symbol.Info,
@@ -21,12 +44,13 @@ namespace qBittorrentCompanion.Converters
                     LogLevel.Error => Symbol.ErrorCircle,
                     LogLevel.Fatal => Symbol.ShieldError,
                     _ => Symbol.QuestionCircle
-                };
-            }
-            else
-                return Symbol.QuestionCircle;
+                }
+                : Symbol.QuestionCircle;
         }
 
+        /// <summary>
+        /// Not supported — returns <see cref="BindingOperations.DoNothing"/>.
+        /// </summary>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => BindingOperations.DoNothing;
     }
