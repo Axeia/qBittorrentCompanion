633aa5cc141b786cc8cf4b8cf68d941770577f91
Pascal Bakhuis <pbakhuis@gmail.com>
2025-10-01 00:23:21 +0100

    Added documentation, improved code readability

--- qBittorrentCompanion/Converters/ReplaceOptionToStringConverter.cs
+++ qBittorrentCompanion/Converters/ReplaceOptionToStringConverter.cs
@@ -1,6 +1,5 @@
 ﻿using Avalonia.Data;
 using Avalonia.Data.Converters;
-using QBittorrent.Client;
 using qBittorrentCompanion.Helpers;
 using qBittorrentCompanion.ViewModels;
 using System;
@@ -8,23 +7,40 @@ using System.Globalization;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a <see cref="RenameOption"/> enum value into a user-friendly description string.
+    /// Used for UI bindings where rename behavior is displayed or selected.
+    /// </summary>
+    /// <remarks>
+    /// Descriptions are centralized in <see cref="DataConverter.ReplaceOptionDescriptions"/>.
+    /// This converter supports one-way conversion only.
+    /// </remarks>
     public class ReplaceOptionToStringConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="RenameOption"/> to its description string.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="RenameOption"/>.</param>
+        /// <param name="targetType">Irrelevant</param>
+        /// <param name="parameter">Irrelevant</param>
+        /// <param name="culture">Irrelevant</param>
+        /// <returns>A description string for the rename option.</returns>
         public object Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is RenameOption replaceOption)
-            {
-                return replaceOption switch
+            return value is RenameOption option
+                ? option switch
                 {
                     RenameOption.Replace => DataConverter.ReplaceOptionDescriptions.Replace,
                     RenameOption.ReplaceOneByOne => DataConverter.ReplaceOptionDescriptions.ReplaceOneByOne,
                     RenameOption.ReplaceAll => DataConverter.ReplaceOptionDescriptions.ReplaceAll,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-            }
-            return DataConverter.ReplaceOptionDescriptions.Replace;
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown RenameOption")
+                }
+                : DataConverter.ReplaceOptionDescriptions.Replace;
         }
 
+        /// <summary>
+        /// Not supported — returns <see cref="BindingOperations.DoNothing"/>.
+        /// </summary>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
             return BindingOperations.DoNothing;
