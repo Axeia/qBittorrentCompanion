1e052a0b571a100096aa3e1f8669472dacb8a8f2
Pascal Bakhuis <pbakhuis@gmail.com>
2025-10-01 00:36:01 +0100

    Added documentation, improved code readability

--- qBittorrentCompanion/Converters/UploadChokingAlgorithmConverter.cs
+++ qBittorrentCompanion/Converters/UploadChokingAlgorithmConverter.cs
@@ -1,50 +1,59 @@
 ﻿using Avalonia.Data;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
 using QBittorrent.Client;
-using qBittorrentCompanion.Helpers;
-using qBittorrentCompanion.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static qBittorrentCompanion.Helpers.DataConverter;
 
 namespace qBittorrentCompanion.Converters
 {
+    /// <summary>
+    /// Converts a <see cref="SeedChokingAlgorithm"/> enum value into a user-friendly description string,
+    /// and vice versa. Used for UI bindings where upload choking behavior is displayed or selected.
+    /// </summary>
+    /// <remarks>
+    /// Descriptions are centralized in <see cref="UploadChokingAlgorithms"/>.
+    /// This converter supports both forward and reverse mapping.
+    /// </remarks>
     public class UploadChokingAlgorithmConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="SeedChokingAlgorithm"/> to its description string.
+        /// </summary>
+        /// <param name="value">Expected to be a <see cref="SeedChokingAlgorithm"/>.</param>
+        /// <returns>A description string for the algorithm, or default if input is invalid.</returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
-            if (value is SeedChokingAlgorithm seedChokingAlgorithm)
-            {
-                return seedChokingAlgorithm switch
+            return value is SeedChokingAlgorithm algorithm
+                ? algorithm switch
                 {
                     SeedChokingAlgorithm.RoundRobin => UploadChokingAlgorithms.RoundRobin,
                     SeedChokingAlgorithm.FastestUpload => UploadChokingAlgorithms.FastestUpload,
                     SeedChokingAlgorithm.AntiLeech => UploadChokingAlgorithms.AntiLeech,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-            }
-            return UploadChokingAlgorithms.RoundRobin;
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown SeedChokingAlgorithm")
+                }
+                : UploadChokingAlgorithms.RoundRobin;
         }
+
+        /// <summary>
+        /// Converts a description string back into a <see cref="SeedChokingAlgorithm"/>.
+        /// </summary>
+        /// <param name="value">Expected to be a description string.</param>
+        /// <returns>The corresponding <see cref="SeedChokingAlgorithm"/>, or throws if unknown.</returns>
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo? culture)
         {
             if (value is string str)
             {
-                if (str == DataConverter.UploadChokingAlgorithms.RoundRobin)
-                    return SeedChokingAlgorithm.RoundRobin;
-                if (str == DataConverter.UploadChokingAlgorithms.FastestUpload)
-                    return SeedChokingAlgorithm.FastestUpload;
-                if (str == DataConverter.UploadChokingAlgorithms.AntiLeech)
-                    return SeedChokingAlgorithm.AntiLeech;
-
-                throw new ArgumentOutOfRangeException();
+                return str switch
+                {
+                    var s when s == UploadChokingAlgorithms.RoundRobin => SeedChokingAlgorithm.RoundRobin,
+                    var s when s == UploadChokingAlgorithms.FastestUpload => SeedChokingAlgorithm.FastestUpload,
+                    var s when s == UploadChokingAlgorithms.AntiLeech => SeedChokingAlgorithm.AntiLeech,
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown upload choking description")
+                };
             }
 
-            return BindingOperations.DoNothing; ;
+            return BindingOperations.DoNothing;
         }
     }
 }
