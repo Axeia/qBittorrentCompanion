18230308a26db401f9a3fdf7b9bcb0b047e5c25d
Pascal Bakhuis <pbakhuis@gmail.com>
2025-11-13 05:08:29 +0000

    Adds reactive IsSelected property to `Tag`

---
+++ qBittorrentCompanion/Helpers/SelectableTag.cs
@@ -0,0 +1,14 @@
+﻿using AutoPropertyChangedGenerator;
+using ReactiveUI;
+
+namespace qBittorrentCompanion.Helpers
+{
+    public partial class SelectableTag(string tag) : ReactiveObject
+    {
+        [AutoPropertyChanged]
+        private string _name = tag;
+
+        [AutoPropertyChanged]
+        private bool _isSelected;
+    }
+}
