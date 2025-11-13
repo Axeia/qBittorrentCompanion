048da3375a14a7d080d34e859c22ecae80ccd264
Pascal Bakhuis <pbakhuis@gmail.com>
2025-11-13 05:08:20 +0000

    Adds reactive IsSelected property to `Category`

---
+++ qBittorrentCompanion/Helpers/SelectableCategory.cs
@@ -0,0 +1,16 @@
+﻿using AutoPropertyChangedGenerator;
+using QBittorrent.Client;
+using ReactiveUI;
+
+namespace qBittorrentCompanion.Helpers
+{
+    public partial class SelectableCategory(Category category) : ReactiveObject
+    {
+        private readonly Category _category = category;
+
+        public string Name => _category.Name;
+
+        [AutoPropertyChanged]
+        private bool _isSelected;
+    }
+}
