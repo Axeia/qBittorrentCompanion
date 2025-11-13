6fc27c5bbdcd2165653a16234182d0d413fa19df
Pascal Bakhuis <pbakhuis@gmail.com>
2025-11-13 05:13:13 +0000

    Contains the most basic of information for adding a torrent

---
+++ qBittorrentCompanion/ViewModels/AddTorrentsRequestViewModel.cs
@@ -0,0 +1,26 @@
+﻿using QBittorrent.Client;
+using System;
+using System.Collections.ObjectModel;
+
+namespace qBittorrentCompanion.ViewModels
+{
+    public partial class AddTorrentsRequestViewModel : ViewModelBase
+    {
+        private AddTorrentsRequest _addTorrentsRequest;
+
+        public AddTorrentsRequestViewModel(AddTorrentsRequest addTorrentsRequest)
+        {
+            _addTorrentsRequest = addTorrentsRequest;
+
+            foreach (string filePath in addTorrentsRequest.TorrentFiles)
+                TorrentFiles.Add(filePath);
+            foreach (Uri uri in addTorrentsRequest.TorrentUrls)
+                TorrentUrls.Add(uri);
+        }
+
+        public ObservableCollection<string> TorrentFiles = [];
+        public ObservableCollection<Uri> TorrentUrls = [];
+
+        public string? DownloadFolder = null;
+    }
+}
