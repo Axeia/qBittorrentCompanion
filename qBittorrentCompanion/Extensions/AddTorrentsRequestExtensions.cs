b24809b864383e6127e0ab07217c4aba3dfde891
Pascal Bakhuis <pbakhuis@gmail.com>
2025-11-16 18:26:41 +0000

    Adds convenience methods to batch Files/URLs

---
+++ qBittorrentCompanion/Extensions/AddTorrentsRequestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using QBittorrent.Client;
+using System;
+using System.Collections.Generic;
+
+namespace qBittorrentCompanion.Extensions
+{
+    public static class AddTorrentsRequestExtensions
+    {
+        public static void BatchAddFiles(this AddTorrentsRequest addTorrentsRequest, IEnumerable<string> filePaths)
+        {
+            foreach (var filePath in filePaths)
+                addTorrentsRequest.TorrentFiles.Add(filePath);
+        }
+        public static void BatchAddUrls(this AddTorrentsRequest addTorrentsRequest, IEnumerable<Uri> urls)
+        {
+            foreach (var url in urls)
+                addTorrentsRequest.TorrentUrls.Add(url);
+        }
+    }
+}
