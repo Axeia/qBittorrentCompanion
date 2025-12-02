90ce813229316dc673e7e4ac468b3c2c3dd7387d
Pascal Bakhuis <pbakhuis@gmail.com>
2025-12-02 02:59:25 +0000

    Added properties for relevant paths
    Fields save to ConfigService on change

--- qBittorrentCompanion/ViewModels/LocalSettings/LaunchFilesViewModel.cs
+++ qBittorrentCompanion/ViewModels/LocalSettings/LaunchFilesViewModel.cs
@@ -1,9 +1,45 @@
-﻿namespace qBittorrentCompanion.ViewModels.LocalSettings
-{
-    public partial class LaunchFilesViewModel : ViewModelBase
-    {
+﻿using AutoPropertyChangedGenerator;
+using Avalonia.Controls;
+using qBittorrentCompanion.Services;
+using ReactiveUI;
 
-        //        ConfigService.TemporaryDirectory = TemporaryDirectoryTextBox.Text;
-        //        ConfigService.DownloadDirectory = DownloadDirectoryTextBox.Text;
+namespace qBittorrentCompanion.ViewModels.LocalSettings
+{
+    public class LaunchFilesViewModel : ViewModelBase
+    {
+        private string _downloadDirectoryPath = Design.IsDesignMode
+            ? string.Empty
+            : ConfigService.DownloadDirectory;
+        public string DownloadDirectoryPath
+        {
+            get => _downloadDirectoryPath;
+            set
+            {
+                if (value != _downloadDirectoryPath)
+                {
+                    _downloadDirectoryPath = value;
+                    ConfigService.DownloadDirectory = value;
+                    this.RaisePropertyChanged(nameof(DownloadDirectoryPath));
+                }
+            }
+        }
+
+
+        private string _temporaryDirectoryPath = Design.IsDesignMode
+            ? string.Empty
+            : ConfigService.TemporaryDirectory;
+        public string TemporaryDirectoryPath
+        {
+            get => _temporaryDirectoryPath;
+            set
+            {
+                if (value != _temporaryDirectoryPath)
+                {
+                    _temporaryDirectoryPath = value;
+                    ConfigService.TemporaryDirectory = value;
+                    this.RaisePropertyChanged(nameof(TemporaryDirectoryPath));
+                }
+            }
+        }
     }
 }
