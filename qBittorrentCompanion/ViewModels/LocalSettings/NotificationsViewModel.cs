5273cd71891bd771c4d7eb861a3d423288bf3e39
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-03 04:08:14 +0000

    Added ViewModel to NotificationsView
    Also updated the UI to reflect planned features

---
+++ qBittorrentCompanion/ViewModels/LocalSettings/NotificationsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using qBittorrentCompanion.Services;
+using ReactiveUI;
+
+namespace qBittorrentCompanion.ViewModels.LocalSettings
+{
+    public partial class NotificationsViewModel : ViewModelBase
+    {
+        private bool _notificationNativeDisconnected = Design.IsDesignMode
+            ? false
+            : ConfigService.NotificationNativeDisconnected;
+
+        public bool NotificationNativeDisconnected
+        {
+            get => _notificationNativeDisconnected;
+            set
+            {
+                if (_notificationNativeDisconnected != value)
+                {
+                    _notificationNativeDisconnected = value;
+                    ConfigService.NotificationNativeDisconnected = value;
+                    this.RaisePropertyChanged(nameof(NotificationNativeDisconnected));
+                }
+            }
+        }
+    }
+}
--- qBittorrentCompanion/Views/LocalSettings/NotificationsView.axaml.cs
+++ qBittorrentCompanion/Views/LocalSettings/NotificationsView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Labs.Notifications;
+using qBittorrentCompanion.ViewModels.LocalSettings;
+using System.Diagnostics;
 
 namespace qBittorrentCompanion.Views.LocalSettings
 {
@@ -7,6 +10,31 @@ namespace qBittorrentCompanion.Views.LocalSettings
         public NotificationsView()
         {
             InitializeComponent();
+            DataContext = new NotificationsViewModel();
+        }
+
+        private void TestNotification_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (NativeNotificationManager.Current is { } manager)
+            {
+                INativeNotification? inn = manager.CreateNotification("Test");
+                if (inn is INativeNotification innn)
+                {
+                    innn.Message = "Leap-16.0-offline-installer-x86_64-Build171.1.install.iso";
+                    innn.Title = "Download completed";
+                    innn.Tag = "qBittorrent Companion";
+                    innn.Icon = App.Current?.CurrentModeWindowIconBitmap;
+                    innn.Show();
+                }
+                else
+                {
+                    Debug.WriteLine("No native notification");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("No native notification manager");
+            }
         }
     }
 }
