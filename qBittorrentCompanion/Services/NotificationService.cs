04714edb7cc17eb16263a3674c20097c2f271649
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-03 04:07:05 +0000

    Service for accessing NotificationNativeSuspended

---
+++ qBittorrentCompanion/Services/NotificationService.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System;
+
+namespace qBittorrentCompanion.Services
+{
+    // Class to centralize access to NotificationNativeSuspended settings
+    public partial class NotificationService : ReactiveObject
+    {
+        private static readonly Lazy<NotificationService> _instance = new(() => new());
+        public static NotificationService Instance => _instance.Value;
+
+        private bool _notificationNativeSuspended = !Design.IsDesignMode && ConfigService.NotificationNativeSuspended;
+
+        public bool NotificationNativeSuspended
+        {
+            get => _notificationNativeSuspended;
+            set
+            {
+                if (_notificationNativeSuspended != value)
+                {
+                    _notificationNativeSuspended = value;
+                    ConfigService.NotificationNativeSuspended = value;
+                    this.RaisePropertyChanged(nameof(NotificationNativeSuspended));
+                }
+            }
+        }
+    }
+}
