58c4d282e20fad43ca7fabb7b8462d99d1009673
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-05 03:11:59 +0000

    Reponsibilities moved to NotificationService.

--- qBittorrentCompanion/ViewModels/LocalSettings/NotificationsViewModel.cs
+++ qBittorrentCompanion/ViewModels/LocalSettings/NotificationsViewModel.cs
@@ -6,22 +6,5 @@ namespace qBittorrentCompanion.ViewModels.LocalSettings
 {
     public partial class NotificationsViewModel : ViewModelBase
     {
-        private bool _notificationNativeDisconnected = Design.IsDesignMode
-            ? false
-            : ConfigService.NotificationNativeDisconnected;
-
-        public bool NotificationNativeDisconnected
-        {
-            get => _notificationNativeDisconnected;
-            set
-            {
-                if (_notificationNativeDisconnected != value)
-                {
-                    _notificationNativeDisconnected = value;
-                    ConfigService.NotificationNativeDisconnected = value;
-                    this.RaisePropertyChanged(nameof(NotificationNativeDisconnected));
-                }
-            }
-        }
     }
 }
