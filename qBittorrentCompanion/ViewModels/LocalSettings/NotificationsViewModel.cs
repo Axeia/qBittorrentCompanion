32fa1f039f3f066443ba20a13e62a8ced2d2eb43
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-08 02:15:31 +0000

    Added code for in app test notification

--- qBittorrentCompanion/ViewModels/LocalSettings/NotificationsViewModel.cs
+++ qBittorrentCompanion/ViewModels/LocalSettings/NotificationsViewModel.cs
@@ -1,10 +1,28 @@
-﻿using Avalonia.Controls;
-using qBittorrentCompanion.Services;
+﻿using Avalonia.Media;
+using Avalonia.Notification;
+using qBittorrentCompanion.Helpers;
 using ReactiveUI;
+using System;
+using System.Reactive;
 
 namespace qBittorrentCompanion.ViewModels.LocalSettings
 {
     public partial class NotificationsViewModel : ViewModelBase
     {
+        public INotificationMessageManager Manager { get; } = new NotificationMessageManager();
+
+
+        public ReactiveCommand<Unit, Unit> ShowTestNotificationCommand
+            => ReactiveCommand.Create(ShowTestNotification);
+
+        public void ShowTestNotification()
+        {
+            Manager
+                .CreateMessage()
+                .Accent(new SolidColorBrush(ThemeColors.SystemAccentLight1))
+                .HasMessage("This is what a notification would look like, although it'd be in the main window. Self destructing in 10 seconds.")
+                .Dismiss().WithDelay(TimeSpan.FromSeconds(10))
+                .Queue();
+        }
     }
 }
