fda7f5796d049bda4b573572a664aa1967052a73
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-26 04:45:55 +0000

    Updated (and renamed) to handle other addresses in addition to IPs

---
+++ qBittorrentCompanion/Validators/ValidIpOrAddressAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace qBittorrentCompanion.Validators
+{
+    public class ValidIpOrAddressAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string str && !string.IsNullOrWhiteSpace(str))
+            {
+                // Accept IP addresses
+                if (IPAddress.TryParse(str, out _))
+                    return ValidationResult.Success;
+
+                // Accept hostnames/domains
+                if (Uri.CheckHostName(str) != UriHostNameType.Unknown)
+                    return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Not a valid IP or domain name (e.g. 192.168.1.100 or example.com).");
+        }
+    }
+}
