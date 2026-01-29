eb107c862cc56a7b94dd7ff1154a1530da67049e
Pascal Bakhuis <pbakhuis@gmail.com>
2026-01-29 04:27:07 +0000

    Extension to easily turn ThemeVariants enum value in ThemeVariant (from Avalonia.Styling)

---
+++ qBittorrentCompanion/Extensions/ThemeVariantsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Styling;
+using qBittorrentCompanion.ViewModels;
+
+namespace qBittorrentCompanion.Extensions
+{
+    public static class ThemeVariantsExtensions
+    {
+        public static ThemeVariant ToActualThemeVariant(this ThemeVariants themeVariants)
+        {
+            if (themeVariants == ThemeVariants.Dark)
+                return ThemeVariant.Dark;
+            else if (themeVariants == ThemeVariants.Light)
+                return ThemeVariant.Light;
+            else
+                return ThemeVariant.Default;
+        }
+    }
+}
