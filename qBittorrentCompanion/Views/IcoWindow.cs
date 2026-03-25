93808cd22ab212d2afb451aa30fff401b4a958ee
Pascal Bakhuis <pbakhuis@gmail.com>
2026-03-25 01:04:17 +0100

    Fix: Actually uses the right icon now
    It did work before, just not as it should. Also greatly simplified the code.

--- qBittorrentCompanion/Views/IcoWindow.cs
+++ qBittorrentCompanion/Views/IcoWindow.cs
@@ -8,34 +8,14 @@ namespace qBittorrentCompanion.Views
     {
         public IcoWindow()
         {
-            if(!Design.IsDesignMode)
-            { 
-                // Update window icon if the dark/light mode icons are updated
+            if (!Design.IsDesignMode)
+            {
                 this.Bind(IconProperty, new Binding
                 {
                     Source = App.Current,
-                    Path = nameof(App.DarkModeWindowIcon),
+                    Path = nameof(App.CurrentModeWindowIcon),
                     Mode = BindingMode.OneWay
                 });
-                this.Bind(IconProperty, new Binding
-                {
-                    Source = App.Current,
-                    Path = nameof(App.LightModeWindowIcon),
-                    Mode = BindingMode.OneWay
-                });
-
-                this.Icon = App.Current?.CurrentModeWindowIcon;
-
-                // Update window icon if the theme is changed
-                ActualThemeVariantChanged += (_, args) =>
-                {
-                    if (App.Current is App app)
-                    {
-                        app.CurrentModeWindowIcon = ActualThemeVariant == ThemeVariant.Dark
-                            ? app.DarkModeWindowIcon
-                            : app.LightModeWindowIcon;
-                    }
-                };
             }
         }
     }
